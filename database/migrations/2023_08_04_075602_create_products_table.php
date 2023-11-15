6ef8b2ff42b5a0dc0f8f4d7d6616f59e44f04ab7
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2023-11-15 15:18:32 +0100

    Build and seeder

--- database/migrations/2023_08_04_075602_create_products_table.php
+++ database/migrations/2023_08_04_075602_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 2000);
+            $table->string('title', 10);
             $table->string('slug', 2000);
             $table->string('image', 2000)->nullable();
             $table->string('image_mime')->nullable();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // AdminUserSeeder::class,
-            // CountrySeeder::class,
-            // ProductSeeder::class
-            CategoriesSeeder::class
+            AdminUserSeeder::class,
+            CountrySeeder::class,
+            ProductSeeder::class,
+            CategoriesSeeder::class,
         ]);
     }
 }
