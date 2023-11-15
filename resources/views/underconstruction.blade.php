5c1784f3fdfe92fc5216a75536425fc543744c2a
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2023-11-15 21:12:55 +0100

    Under construccion page 2

--- resources/views/underconstruction.blade.php
+++ resources/views/underconstruction.blade.php
@@ -20,24 +20,20 @@
         <div class="enconstruccion">
             <x-application-logo class="block fill-current" />
         </div>
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-            <div class="flex flex-col gap-16 md:flex-row">
-                <div class="w-full md:w-1/2 grid grid-cols-1 gap-x-8 gap-y-6 lg:pt-2 text-center">
-                    <h2 class="text-3xl font-bold sm:text-4xl">Página en construcción</h2>
-                    <hr class="h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
-                    <p class="mt-4 text-lg leading-8 text-gray-400">Encontranos en el local de Av. Ramón y Cajal 10, Fuengirola, Málaga</p>
-                    <div class="flex gap-6 social-icons justify-center">
-                        <a href="https://wa.me/34622406965" class="h-10 w-10 aspect-square rounded-md bg-black/5 p-2 ring-1 ring-black/10" target="_blank">
-                            <i class="flex text-2xl leading-none fi fi-brands-whatsapp"></i>
-                        </a>
-                        <a href="https://www.instagram.com/puntosurfuengirola/?hl=es" class="h-10 w-10 aspect-square rounded-md bg-black/5 p-2 ring-1 ring-black/10" target="_blank">
-                            <i class="flex text-2xl leading-none fi fi-brands-instagram"></i>
-                        </a>
-                        <a href="https://maps.app.goo.gl/22GUnZ2foJeEYud98" class="h-10 w-10 aspect-square rounded-md bg-black/5 p-2 ring-1 ring-black/10" target="_blank">
-                            <i class="flex text-2xl leading-none fi fi-rs-map-marker"></i>
-                        </a>
-                    </div>
-                </div>
+        <div class="w-full grid grid-cols-1 gap-x-8 gap-y-8 text-center">
+            <h2 class="text-3xl font-bold sm:text-4xl">Página en construcción</h2>
+            <hr class="h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
+            <p class="text-lg leading-8 text-gray-400">Encontranos en el local de Av. Ramón y Cajal 10, Fuengirola, Málaga</p>
+            <div class="flex gap-6 social-icons justify-center">
+                <a href="https://wa.me/34622406965" class="h-10 w-10 aspect-square rounded-md bg-black/5 p-2 ring-1 ring-black/10" target="_blank">
+                    <i class="flex text-2xl leading-none fi fi-brands-whatsapp"></i>
+                </a>
+                <a href="https://www.instagram.com/puntosurfuengirola/?hl=es" class="h-10 w-10 aspect-square rounded-md bg-black/5 p-2 ring-1 ring-black/10" target="_blank">
+                    <i class="flex text-2xl leading-none fi fi-brands-instagram"></i>
+                </a>
+                <a href="https://maps.app.goo.gl/22GUnZ2foJeEYud98" class="h-10 w-10 aspect-square rounded-md bg-black/5 p-2 ring-1 ring-black/10" target="_blank">
+                    <i class="flex text-2xl leading-none fi fi-rs-map-marker"></i>
+                </a>
             </div>
         </div>
     </main>
