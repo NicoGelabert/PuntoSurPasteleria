c2457cb708dc0b4372c11325a674d2fead5e12f8
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2023-11-23 11:20:46 +0100

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title inertia>{{ config('app.name', 'Punto Sur Pastelería') }}</title>
 
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('storage/img/favicon.ico') }}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=albert-sans:200,300,500,700|battambang:100,300,400,700,900" rel="stylesheet" />
